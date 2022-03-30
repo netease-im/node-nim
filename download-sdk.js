57ad7b957dd57232d1d40fcd43b910163bababb7
Zvicii <zouyiqi@corp.netease.com>
2022-03-30 11:45:34 +0800

    feat: allow specifying sdk download address.

--- download-sdk.js
+++ download-sdk.js
@@ -7,6 +7,7 @@ const platform = process.platform;
 const sdk_group = "message";
 const sdk_name = "nim";
 const sdk_path = "sdk";
+let sdk_url = process.env.NIM_SDK_URL;
 let sdk_version = package_json.version;
 if (process.env.NIM_SDK_VERSION) {
     sdk_version = process.env.NIM_SDK_VERSION;
@@ -14,20 +15,21 @@ if (process.env.NIM_SDK_VERSION) {
 // fetch publish list
 fetch('http://publish.netease.im/api/list').then((res) => res.json()).then((publish_json) => {
     // get sdk list
-    let sdk_list = [];
-    Object.keys(publish_json[sdk_group]).forEach((temp) => {
-        if (sdk_version.split('-')[0] === temp) {
-            sdk_list = publish_json[sdk_group][temp];
-        }
-    });
-    console.log(`[node_pre_build] downloadSDK sdk_name:${sdk_name}, platform:${platform}, arch:${arch}`)
-    let sdk_url;
-    // use platform and arch to find the sdk
-    sdk_list.forEach((member) => {
-        if (member.filename.includes(sdk_name) && member.filename.includes(platform) && member.filename.includes(arch)) {
-            sdk_url = member.cdnlink;
-        }
-    });
+    if (!sdk_url) {
+        let sdk_list = [];
+        Object.keys(publish_json[sdk_group]).forEach((temp) => {
+            if (sdk_version.split('-')[0] === temp) {
+                sdk_list = publish_json[sdk_group][temp];
+            }
+        });
+        console.log(`[node_pre_build] downloadSDK sdk_name:${sdk_name}, platform:${platform}, arch:${arch}`)
+        // use platform and arch to find the sdk
+        sdk_list.forEach((member) => {
+            if (member.filename.includes(sdk_name) && member.filename.includes(platform) && member.filename.includes(arch)) {
+                sdk_url = member.cdnlink;
+            }
+        });
+    }
     if (!sdk_url) {
         console.error(`[node_pre_build] downloadSDK sdk_name:${sdk_name}, platform:${platform}, arch:${arch} not found`);
         return;
