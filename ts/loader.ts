7133df52176f7ec0ce4429ebd59a68691eda50c6
Zvicii <zouyiqi@corp.netease.com>
2022-05-18 11:33:35 +0800

    perf: add process cwd and exec dir to node search path.

--- ts/loader.ts
+++ ts/loader.ts
@@ -1,24 +1,23 @@
 import fs from 'fs'
 import path from 'path'
 let sdk_path = ''
-let test_sdk_path = ''
 
 // Add the compiled product path to the environment variable,
 // so that the third party does not need to copy the
 // dynamic library separately to the program directory when importing.
 if (process.platform === 'win32') {
     sdk_path = path.join(__dirname, '../sdk/bin')
-    test_sdk_path = path.join(__dirname, '../../../bin')
-    process.env.PATH = `${sdk_path};${test_sdk_path};${process.env.PATH}`
+    process.env.PATH = `${sdk_path};${process.env.PATH}`
 } else if (process.platform === 'darwin') {
     sdk_path = path.join(__dirname, '../sdk/lib')
-    test_sdk_path = path.join(__dirname, '../../../lib')
 }
-let node_nim_path
-if (fs.existsSync(sdk_path)) {
-    node_nim_path = path.join(sdk_path, 'node-nim')
-} else {
-    node_nim_path = path.join(test_sdk_path, 'node-nim')
+let paths = [path.join(sdk_path, 'node-nim.node'), path.join(process.cwd(), 'node-nim.node'), path.join(path.dirname(process.execPath), 'node-nim.node')]
+let node_nim_path = ''
+for (let path of paths) {
+    if (fs.existsSync(path)) {
+        node_nim_path = path
+        break
+    }
 }
 
 export default require(node_nim_path)
