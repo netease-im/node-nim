c65589f6e0a4fc41c157f6a2328c80d68d2c4cdf
Zvicii <zouyiqi@corp.netease.com>
2022-05-24 15:15:57 +0800

    perf: use git-describe to get exact tag

--- script/auto-publish.mjs
+++ script/auto-publish.mjs
@@ -1,11 +1,15 @@
-import git from 'git-rev-sync'
+import git_describe from 'git-describe'
 import { exec } from 'child_process'
 
-const git_tag = git.tag()
-exec(`npm --no-git-tag-version version ${git_tag}`, (err, stdout, stderr) => {
+const git_info = git_describe.gitDescribeSync({ match: "*", customArguments: ["--exact-match"] })
+if (!git_info.tag) {
+    console.log("No tag found, skipping auto-publish")
+    process.exit(0)
+}
+exec(`npm --no-git-tag-version version ${git_info.tag}`, (err, stdout, stderr) => {
     if (err) {
         console.error(err)
         return
     }
-    exec(`npm publish`)
+    exec(`npm publish --dry-run`)
 })
